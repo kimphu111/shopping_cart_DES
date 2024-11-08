d253a8425f3d84a7e9ab8fa5b0d01a5a097a0bc7
kimphu <phu.hhk05004@sinhvien.hoasen.edu.vn>
2024-11-08 08:19:22 +0700

    Done searching prods

--- client/src/app/pages/album-1/album-1.component.ts
+++ client/src/app/pages/album-1/album-1.component.ts
@@ -1,24 +1,34 @@
 import { Component } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
+import {CurrencyPipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-album-1',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    NgForOf,
+    NgIf,
+    CurrencyPipe
   ],
   templateUrl: './album-1.component.html',
   styleUrl: './album-1.component.scss'
 })
 export class Album1_Component {
-  products = [
-    { name: 'Áo sơ mi', price: '500,000₫' },
-    { name: 'Quần jean', price: '600,000₫' },
-    { name: 'Giày thể thao', price: '1,200,000₫' },
-    { name: 'Áo khoác', price: '800,000₫' },
-    { name: 'Nón', price: '200,000₫' },
-    { name: 'Balo', price: '750,000₫' }
-  ];
+      products = [
+        { id: 1, name: 'Áo sơ mi nam', price: 100000, image: 'https://via.placeholder.com/150' },
+        { id: 2, name: 'Áo thun nam', price: 200000, image: 'https://via.placeholder.com/150' },
+        { id: 3, name: 'Áo khoác nam', price: 300000, image: 'https://via.placeholder.com/150' },
+        { id: 4, name: 'Áo len nam', price: 400000, image: 'https://via.placeholder.com/150' },
+        { id: 5, name: 'Áo phông nam', price: 500000, image: 'https://via.placeholder.com/150' },
+        { id: 6, name: 'Áo sơ mi nữ', price: 600000, image: 'https://via.placeholder.com/150' },
+        { id: 7, name: 'Áo thun nữ', price: 700000, image: 'https://via.placeholder.com/150' },
+        { id: 8, name: 'Áo khoác nữ', price: 800000, image: 'https://via.placeholder.com/150' },
+        { id: 9, name: 'Áo len nữ', price: 900000, image: 'https://via.placeholder.com/150' },
+        { id: 10, name: 'Áo phông nữ', price: 1000000, image: 'https://via.placeholder.com/150' },
+        { id: 11, name: 'Áo sơ mi trẻ em', price: 1100000, image: 'https://via.placeholder.com/150' },
+        { id: 12, name: 'Áo thun trẻ em', price: 1200000, image: 'https://via.placeholder.com/150' }
+      ];
 
   // Biến để lưu kết quả tìm kiếm
   filteredProducts = this.products;
--- client/src/app/pages/album-2/album-2.component.ts
+++ client/src/app/pages/album-2/album-2.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {CurrencyPipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-album-2',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    CurrencyPipe,
+    NgForOf
+  ],
   templateUrl: './album-2.component.html',
   styleUrl: './album-2.component.scss'
 })
 export class Album2_Component {
+  //tạo 12 món phu kien
+    products = [
+        { id: 1, name: 'Túi xách nữ', price: 100000, image: 'https://via.placeholder.com/150' },
+        { id: 2, name: 'Túi đeo chéo nữ', price: 200000, image: 'https://via.placeholder.com/150' },
+        { id: 3, name: 'Túi xách nam', price: 300000, image: 'https://via.placeholder.com/150' },
+        { id: 4, name: 'Túi đeo chéo nam', price: 400000, image: 'https://via.placeholder.com/150' },
+        { id: 5, name: 'Mũ nón nữ', price: 500000, image: 'https://via.placeholder.com/150' },
+        { id: 6, name: 'Mũ nón nam', price: 600000, image: 'https://via.placeholder.com/150' },
+        { id: 7, name: 'Kính mát nữ', price: 700000, image: 'https://via.placeholder.com/150' },
+        { id: 8, name: 'Kính mát nam', price: 800000, image: 'https://via.placeholder.com/150' },
+        { id: 9, name: 'Dây chuyền nữ', price: 900000, image: 'https://via.placeholder.com/150' },
+        { id: 10, name: 'Dây chuyền nam', price: 1000000, image: 'https://via.placeholder.com/150' },
+        { id: 11, name: 'Vòng cổ nữ', price: 1100000, image: 'https://via.placeholder.com/150' },
+        { id: 12, name: 'Vòng cổ nam', price: 1200000, image: 'https://via.placeholder.com/150' }
+      ];
 
+
+
+// biến lưu kết quả tìm kiếm
+  filteredProducts = this.products;
+
+// Hàm lọc sản phẩm dựa theo
+  searchProducts(searchTerm: string) {
+    if (searchTerm) {
+      this.filteredProducts = this.products.filter(product =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredProducts = this.products;
+    }
+  }
 }
