9392c26385925a4a2412ac81e1ee58c922d1c79d
kimphu <phu.hhk05004@sinhvien.hoasen.edu.vn>
2024-12-07 17:44:42 +0700

    fixed window SSR

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const port = 8000;
 const https = require("https");
 const fs = require("fs");
 
-const options = {
-
-  key: fs.readFileSync('./key.pem'),
-
-  cert: fs.readFileSync('./cert.pem'),
-
-};
-const server = https.createServer(options, app);
+// const options = {
+//
+//   // key: fs.readFileSync('./key.pem'),
+//   //
+//   // cert: fs.readFileSync('./cert.pem'),
+//
+// };
+// const server = https.createServer(options, app);
 
 
 
